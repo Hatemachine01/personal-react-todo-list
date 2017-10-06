367f55582263a2397030b669411cf29ded10580b
Julio <juliodevasquez@gmail.com>
2017-10-06 18:39:23 -0500

    created all components

--- src/Timer/components/Timer.jsx
+++ src/Timer/components/Timer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../TimerHeader/components/TimerHeader";
 import Display from "../../TimerDisplay/components/TimerDisplay";
 import TimerButton from "../../TimerButton/components/TimerButton";
+import TimerConfig from "../../TimerConfig/components/TimerConfig";
 
 const Timer = ()=> (
 
@@ -9,39 +10,9 @@ const Timer = ()=> (
               <Header />
               <Display />
               <TimerButton  />
-                <div className="row">
-                 <h2 className="text-primary"> Set Timer</h2>
-                <div className="row control-row ">
-                  <div className="form-group">
-                    <div className="col-sm-3">
-                      <label htmlFor="hours">Hours </label>
-                    </div>
-                    <div className="col-sm-9">
-                    <input id="hours" className="form-control" type="number"/>
-                    </div>
-                  </div>
-                  </div>
-                  <div className="row control-row">
-                  <div className="form-group">
-                    <div className="col-sm-3">
-                      <label htmlFor="minutes"> Minutes </label>
-                    </div>
-                    <div className="col-sm-9">
-                    <input id="hours" className="form-control" type="number"/>
-                    </div>
-                  </div>
-                  <div className="row control-row">
-                    </div><div className="form-group">
-                      <div className="col-sm-3">
-                        <label htmlFor="seconds"> Seconds  </label>
-                      </div>
-                      <div className="col-sm-9">
-                      <input id="hours" className="form-control" type="number"/>
-                      </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+              <TimerConfig  />
+               
+</div>
 )
 
 export default Timer;
